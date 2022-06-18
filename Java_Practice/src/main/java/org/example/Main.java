c62b0173e94acd534814748ca6cf1b671d440208
rkadl9999 <rkadl9999@naver.com>
2022-06-18 22:39:56 +0900

    자바 문법 공부

---
+++ Java_Practice/src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //데이터 출력
+        System.out.println("Hello world!"); //데이터 출력 후, 개행
+        System.out.print("Hello world!"); //데이터 출력.
+        
+
+    }
+}
