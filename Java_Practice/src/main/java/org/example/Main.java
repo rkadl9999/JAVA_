ee2fa7cce1a00471a7736dfe085ff139c183c392
rkadl9999 <rkadl9999@naver.com>
2022-06-18 22:46:30 +0900

    데이터 입출력

--- Java_Practice/src/main/java/org/example/Main.java
+++ Java_Practice/src/main/java/org/example/Main.java
@@ -1,11 +1,19 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         //데이터 출력
         System.out.println("Hello world!"); //데이터 출력 후, 개행
         System.out.print("Hello world!"); //데이터 출력.
-        
+
+        //데이터 입력
+        Scanner sc = new Scanner(System.in); //Scanner 객체 생성 ( sc )
+        String name = sc.next(); //name 변수에 값 저장
+        int age = sc.nextInt(); //age 변수에 값 저장
+        System.out.println("이름 : "+name+" 나이 : "+age); //입력 받은 값 출력
+
 
     }
 }
