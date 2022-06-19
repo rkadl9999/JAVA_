5c9eedc7f14849367c9fc2464b6c6c89a02596a3
rkadl9999 <rkadl9999@naver.com>
2022-06-19 22:10:33 +0900

    ✨ | 제어자 문법 정리

---
+++ Java_Practice/src/main/java/org/example/Modifier.java
@@ -0,0 +1,19 @@
+package org.example;
+
+public class Modifier {
+    public static void main(String[] args) {
+        //제어자 ( Modifier )
+        //클래스와 클래스의 멤버에 부가적인 의미
+        //접근제어자 | public, protected, (default), private
+        //그 외 | static, final, abstract, ...
+        //하나의 대상에 여러 제어자를 같이 사용 가능 ( 접근 제어자는 오직 하나만 가능 )
+            //static | 클래스의, 공통적인
+            //final | 마지막의, 변경될 수 없는 ( 상수 )
+            //abstract | 추상의, 미완성의
+        //접근 제어자
+            //private | 같은 클래스에서만 접근 가능
+            //(default) | 같은 패키지 안에서만 접근 가능
+            //protected | 같은 패키지, 다른 패키지의 자손 클래스에서만 접근 가능
+            //public | 잡근 제한이 전혀 없다.
+    }
+}
